68ad5e107baaa718dd6178744519c14e00e5f2bf
zhangjw <zgjgwitk@qq.com>
2018-07-31 23:36:35 +0800

    07-31

--- src/zhang/testpackage/BRRead.java
+++ src/zhang/testpackage/BRRead.java
@@ -1,22 +1,22 @@
 package zhang.testpackage;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class BRRead {
-    public static void main(String[] args){
-        char c=' ';
+    public static void main(String[] args) throws Exception {
+        char c = ' ';
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("input any thing, while 'q' exit.");
-        do{
-            try {
-                c=(char) br.read();
+        do {
+            //try {
+            c = (char) br.read();
                 System.out.print(c);
-            } catch (IOException e) {
-                e.printStackTrace();
-                System.out.print("exception happened");
-            }
+            //} catch (IOException e) {
+            //    e.printStackTrace();
+            //    System.out.print("exception happened");
+            //}
 
-        }while(c!='q');
-
+        } while (c != 'q');
     }
 }
